58a99efcaac1f3c0abc1809cac96863746e1f025
Dhamayandhi2004 <126405025+Dhamayandhi2004@users.noreply.github.com>
2024-12-28 15:47:11 +0530

    Adding hoisting.js

---
+++ JavaScript/hoisting.js
@@ -0,0 +1,16 @@
+var a=10;
+console.log(a);
+//eg1
+console.log(a);
+var a=10;
+console.log(a);
+
+//eg2
+console.log(b);
+var b=3.14;
+console.log(b);
+
+//eg3
+console.log(c);
+var c=10;
+console.log(c);
